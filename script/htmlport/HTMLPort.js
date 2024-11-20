5a7d7aff46cb0deb30e759f2cf1392c15d1df77d
Alejandro José de Tena Camacho <vulgrim@outlook.es>
2024-11-21 00:02:17 +0100

    Refactor and updates.

--- script/htmlport/HTMLPort.js
+++ script/htmlport/HTMLPort.js
@@ -1,32 +1,45 @@
 import { Page } from "./Page.js";
 
 export class HTMLPort {
+    static #getSettings(report) {
+        const settings = {
+            width: 8.27,
+            height: 11.69,
+            padding: 0.5,
+            pagebreak: "auto"
+        }
+
+        Object
+            .keys(settings)
+            .forEach(key => settings[key] = report.querySelector(`#${key}`)?.innerText ?? settings[key]);
+
+        return settings;
+    }
+
+    static #getParts(report) {
+        return {
+            fixtops: report.querySelectorAll(".fixtop"),
+            headers: report.querySelectorAll(".header"),
+            records: report.querySelectorAll(".record"),
+            appends: report.querySelectorAll(".append"),
+            footers: report.querySelectorAll(".footer"),
+            fixbots: report.querySelectorAll(".fixbot")
+        };
+    }
+
+    static #removeUnusedParts(parts) {
+        Array.from(parts.headers).map(header => header.parentElement.removeChild(header));
+        Array.from(parts.footers).map(footer => footer.parentElement.removeChild(footer));
+    }
+
     static run() {
         const reports = document.querySelectorAll(".report");
 
         reports.forEach(report => {
-            const settings = {
-                width: 8.27,
-                height: 11.69,
-                padding: 0.5,
-                pagebreak: "auto"
-            }
+            const settings = HTMLPort.#getSettings(report);
+            const parts = HTMLPort.#getParts(report);
 
-            Object
-                .keys(settings)
-                .forEach(key => settings[key] = report.querySelector(`#${key}`)?.innerText ?? settings[key]);
-
-            this.parts = {
-                fixtops: report.querySelectorAll(".fixtop"),
-                headers: report.querySelectorAll(".header"),
-                records: report.querySelectorAll(".record"),
-                appends: report.querySelectorAll(".append"),
-                footers: report.querySelectorAll(".footer"),
-                fixbots: report.querySelectorAll(".fixbot")
-            };
-
-            Array.from(this.parts.headers).map(header => header.parentElement.removeChild(header));
-            Array.from(this.parts.footers).map(footer => footer.parentElement.removeChild(footer));
+            HTMLPort.#removeUnusedParts(parts);
 
             let page = new Page(report, settings);
 
@@ -36,36 +49,36 @@ export class HTMLPort {
 
                 page = new Page(report, settings);
 
-                page.useHeaders(this.parts.headers);
-                page.useFooters(this.parts.footers);
+                page.useHeaders(parts.headers);
+                page.useFooters(parts.footers);
             };
 
-            page.useHeaders(this.parts.headers);
-            page.useFooters(this.parts.footers);
+            page.useHeaders(parts.headers);
+            page.useFooters(parts.footers);
 
-            const appendTo = ((to, child, before = null) => {
+            const appendTo = ((to, child) => {
                 if (!page.fits(child)) startNewPage();
 
                 switch (to) {
                     case "head":
-                        page.main.insertBefore(child, page.main.firstChild);
-                        break;
-                    case "main":
-                        page.main.insertBefore(child, before);
+                        page.main.insertBefore(child, page.main.querySelector(".header, .body"));
                         break;
                     case "body":
-                        page.body.insertBefore(child, before);
+                        page.body.insertBefore(child);
+                        break;
+                    case "foot":
+                        page.main.insertBefore(child);
                         break;
                 }
             });
 
-            this.parts.fixtops.forEach(fixtop => appendTo("head", fixtop));
-            this.parts.records.forEach(record => appendTo("body", record));
+            parts.fixtops.forEach(fixtop => appendTo("head", fixtop));
+            parts.records.forEach(record => appendTo("body", record));
 
-            if (!page.fits(...[...this.parts.appends, ...this.parts.fixbots])) startNewPage();
+            if (!page.fits(...[...parts.appends, ...parts.fixbots])) startNewPage();
 
-            this.parts.appends.forEach(append => appendTo("body", append));
-            this.parts.fixbots.forEach(fixbot => appendTo("main", fixbot));
+            parts.appends.forEach(append => appendTo("body", append));
+            parts.fixbots.forEach(fixbot => appendTo("foot", fixbot));
 
             page.fillPage();
             page.setPageNo();
